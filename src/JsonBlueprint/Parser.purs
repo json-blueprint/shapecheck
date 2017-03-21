5377d72cc80192d11a9009cd8bbb39cbe21ccc64
Martin Schmied <mschmied@gmail.com>
2017-03-21 09:10:06 +0100

    rewrite sample parsers with Eulalie

--- src/JsonBlueprint/Parser.purs
+++ src/JsonBlueprint/Parser.purs
@@ -1,65 +1,78 @@
 module JsonBlueprint.Parser where
 
 import Prelude
+import Data.Eulalie.Char as C
+import Data.Eulalie.String as S
 import Control.Alt ((<|>))
 import Data.Array (fromFoldable)
 import Data.Char (fromCharCode)
 import Data.Char.Unicode (isHexDigit)
--- import Data.Eulalie.Parser (Parser(..))
+import Data.Eulalie.Parser (Parser, cut, expected, fail, many, sat)
 import Data.Foldable (class Foldable)
 import Data.Int (fromStringAs, hexadecimal)
 import Data.List.Lazy (replicateM)
 import Data.Maybe (Maybe(..))
 import Data.String (fromCharArray)
 import JsonBlueprint.Pattern (Pattern(..))
-import Text.Parsing.StringParser (Parser, fail, try)
-import Text.Parsing.StringParser.Combinators ((<?>), many)
-import Text.Parsing.StringParser.String (char, noneOf, oneOf, satisfy, string)
 
-stringChar :: Parser Char
-stringChar = noneOf ['\\', '"']
+stringChar :: Parser Char Char
+stringChar =  sat \c -> c /= '\\' && c /= '"'
 
-standardEscape :: Parser Char
-standardEscape = do
-  try $ char '\\'
-  c <- try $ oneOf ['\\', '/', '"', 'b', 'f', 'n', 'r', 't']
-  pure $ case c of
-    'b' -> '\b'
-    'f' -> '\f'
-    'n' -> '\n'
-    'r' -> '\r'
-    't' -> '\t'
-    other -> other
+standardEscape :: Parser Char Char
+standardEscape = expected standardEscape' "standard escape sequence" where
+  standardEscape' = do
+    C.char '\\'
+    c <- C.oneOf "\\/\"bfnrt"
+    pure $ case c of
+      'b' -> '\b'
+      'f' -> '\f'
+      'n' -> '\n'
+      'r' -> '\r'
+      't' -> '\t'
+      other -> other
 
-hexDigit :: Parser Char
-hexDigit = satisfy isHexDigit <?> "hex digit"
+hexDigit :: Parser Char Char
+hexDigit = expected (sat isHexDigit) "hex digit"
 
-unicodeEscape :: Parser Char
-unicodeEscape = do
-    try $ string "\\u"
+unicodeEscape :: Parser Char Char
+unicodeEscape = expected unicodeEscape' "unicode escape"
+
+unicodeEscape' :: Parser Char Char
+unicodeEscape' = do
+    S.string "\\u"
     ds <- replicateM 4 hexDigit
     decodeChar ds
   where
-    decodeChar :: forall f. (Foldable f) => f Char -> Parser Char
+    decodeChar :: forall f. (Foldable f) => f Char -> Parser Char Char
     decodeChar cs =
       let optCharCode = (fromFoldable >>> fromCharArray >>> (fromStringAs hexadecimal)) cs in
         case optCharCode of
           Just charCode -> pure $ fromCharCode charCode
-          Nothing -> fail "invalid unicode escape sequence"
+          Nothing -> fail
 
-stringLiteral :: Parser String
-stringLiteral = do
-  char '"'
-  cs <- many $ stringChar <|> standardEscape <|> unicodeEscape
-  char '"'
-  pure $ fromFoldable >>> fromCharArray $ cs
+booleanDataType :: Parser Char Pattern
+booleanDataType = expected (S.string "Boolean" <#> (\_ -> BooleanDataType)) "boolean data type"
 
-booleanDataType :: Parser Pattern
-booleanDataType = try $ string "Boolean" <#> (\_ -> BooleanDataType)
+booleanLiteral :: Parser Char Pattern
+booleanLiteral = expected booleanLiteral' "boolean literal" where
+  booleanLiteral' = (S.string "true" <#> \_ -> BooleanLiteral true)
+                <|> (S.string "false" <#> \_ -> BooleanLiteral false)
 
-booleanLiteral :: Parser Pattern
-booleanLiteral = (string "true" <#> \_ -> BooleanLiteral true)
-             <|> (string "false" <#> \_ -> BooleanLiteral false)
+stringLiteral :: Parser Char Pattern
+stringLiteral = expected stringLiteral' "string literal" where
+  stringLiteral' = do
+    C.char '"'
+    cs <- cut $ many $ stringChar <|> unicodeEscape <|> standardEscape
+    C.char '"'
+    pure $ StringLiteral $ fromFoldable >>> fromCharArray $ cs
 
-stringDataType :: Parser Pattern
-stringDataType = try $ string  "String" <#> (\_ -> StringDataType { minLength: Nothing, maxLength: Nothing })
+stringDataType :: Parser Char Pattern
+stringDataType = expected stringDataType' "string data type" where
+  stringDataType' = S.string  "String" <#> (\_ -> StringDataType { minLength: Nothing, maxLength: Nothing })
+
+valuePattern :: Parser Char Pattern
+valuePattern =
+  booleanLiteral <|>
+  booleanDataType <|>
+  stringLiteral <|>
+  stringDataType
--- src/JsonBlueprint/Pattern.purs
+++ src/JsonBlueprint/Pattern.purs
@@ -6,7 +6,7 @@ import Data.Maybe (Maybe)
 data Pattern = Empty
              | BooleanLiteral Boolean
              | BooleanDataType
---             | StringLiteral String
+             | StringLiteral String
              | StringDataType { minLength :: Maybe Int, maxLength :: Maybe Int }
 --             | Choice Pattern Pattern
 
@@ -14,7 +14,7 @@ instance showPattern :: Show Pattern where
   show Empty = "Empty"
   show (BooleanLiteral b)    = show b
   show BooleanDataType       = "Boolean"
---  show (StringLiteral str)   = show str
+  show (StringLiteral str)   = show str
   show (StringDataType opts) = "String"           -- TODO: append props
 --  show (Choice p1 p2)        = (show p1) <> " | " <> (show p2)
 
